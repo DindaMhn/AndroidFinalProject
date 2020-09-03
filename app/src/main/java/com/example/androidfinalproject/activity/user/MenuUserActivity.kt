5badb33789c341c094392a0d10bfd2fd9312959e
Dinda Maharani <34880692+DindaMhn@users.noreply.github.com>
2020-09-03 19:03:36 +0700

    [UPDATE] home layout done

---
+++ app/src/main/java/com/example/androidfinalproject/activity/user/MenuUserActivity.kt
@@ -0,0 +1,30 @@
+package com.example.androidfinalproject.activity.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.androidfinalproject.R
+import kotlinx.android.synthetic.main.activity_menu_user.*
+
+class MenuUserActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_user)
+        navController = (nav_host_fragment_menu_user_container as NavHostFragment).navController
+        NavigationUI.setupWithNavController(bottom_navigation_user, navController)
+        bottom_navigation_user.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeUser -> {
+                    navController.navigate(R.id.action_global_homeUserFragment)
+                    true
+                }
+                else ->{
+                    true
+                }
+            }
+        }
+    }
+}
