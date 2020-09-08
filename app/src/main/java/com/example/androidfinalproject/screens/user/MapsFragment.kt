1ada75ac647ff5d863b4932ea8b00ed5b310bf99
juanaf31 <50581538+juanaf31@users.noreply.github.com>
2020-09-08 20:51:27 +0700

    fetching map

--- app/src/main/java/com/example/androidfinalproject/screens/user/MapsFragment.kt
+++ app/src/main/java/com/example/androidfinalproject/screens/user/MapsFragment.kt
@@ -2,12 +2,15 @@ package com.example.androidfinalproject.screens.user
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
 import android.os.Build
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -16,12 +19,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.ContextCompat.getSystemServiceName
 import androidx.lifecycle.Observer
 import com.example.androidfinalproject.MyApplication
 import com.example.androidfinalproject.R
 import com.example.androidfinalproject.user.account.UserViewModel
 import com.example.androidfinalproject.user.search.LocationViewModel
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -36,6 +41,12 @@ class MapsFragment : Fragment() {
     @Inject
     lateinit var locationViewModel: LocationViewModel
     val FINE_LOCATION_RQ = 101
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    var devLong: Double = 0.00
+    var devLat:Double = 0.00
+
+
     private val callback = OnMapReadyCallback { googleMap ->
         locationViewModel.listLocationData.observe(viewLifecycleOwner, Observer {
             println("masuk map")
@@ -47,6 +58,7 @@ class MapsFragment : Fragment() {
             }
         })
         locationViewModel.getAssetLocation()
+        getLastLocation()
 //        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
     }
@@ -54,8 +66,9 @@ class MapsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity?.applicationContext as MyApplication).applicationComponent.inject(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity as Activity)
 
-        checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, "location",FINE_LOCATION_RQ)
+//        checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, "location",FINE_LOCATION_RQ)
 
     }
     override fun onCreateView(
@@ -71,22 +84,65 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
-    fun checkPermission(permission:String, name : String, requestCode :Int){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            when{
-                ContextCompat.checkSelfPermission(context as Activity,permission) == PackageManager.PERMISSION_GRANTED->{
-                    Toast.makeText(activity,"$name permission granted", Toast.LENGTH_SHORT).show()
-                }
-                shouldShowRequestPermissionRationale(permission)-> showDialog(permission,name,requestCode)
-                else->{
 
-                    ActivityCompat.requestPermissions(context as Activity, arrayOf(permission),requestCode)
+    private fun getLastLocation(){
+        if(checkPermission()){
+            if(isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener{task->
+                    var deviceLocation = task.result
+                    if(deviceLocation == null){
+                        getNewLocation()
+                    } else{
+                        devLat = deviceLocation!!.latitude
+                        devLong = deviceLocation!!.longitude
+                    }
                 }
+            }else{
+                Toast.makeText(context,"Please Enable Your Location Service",Toast.LENGTH_SHORT).show()
             }
+        }else{
+            requestPermission()
         }
     }
 
+    private fun getNewLocation(){
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2
+        checkPermission()
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
+    }
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+            var lastLocation = p0.lastLocation
+            devLat = lastLocation!!.latitude
+            devLong= lastLocation!!.longitude
+        }
+    }
 
+    private fun checkPermission():Boolean{
+        if(
+            ActivityCompat.checkSelfPermission(context as Activity,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(context as Activity, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                ){
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(activity as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),FINE_LOCATION_RQ)
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -94,28 +150,58 @@ class MapsFragment : Fragment() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        fun innerCheck(name: String){
-            if(grantResults.isEmpty()||grantResults[0] != PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(context,"$name permission refused", Toast.LENGTH_SHORT).show()
-            }else{
-
-                Toast.makeText(context,"$name permission granted", Toast.LENGTH_SHORT).show()
-
+        if(requestCode == FINE_LOCATION_RQ){
+            if(grantResults.isEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Log.d("Debug","You Have The Permission")
             }
         }
-        when(requestCode){
-            FINE_LOCATION_RQ -> innerCheck("location")
-        }
     }
-    private fun showDialog(permission:String, name : String, requestCode :Int){
-        val builder = AlertDialog.Builder(context as Activity)
-        builder.apply {
-            setMessage("Permission to access your $name is required")
-            setTitle("Permission Required")
-            setPositiveButton("OK"){ dialog, which -> ActivityCompat.requestPermissions(context as Activity, arrayOf(permission),requestCode)
-            }
-            val dialog = builder.create()
-            dialog.show()
-        }
-    }
+
+//    fun checkPermission(permission:String, name : String, requestCode :Int){
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+//            when{
+//                ContextCompat.checkSelfPermission(context as Activity,permission) == PackageManager.PERMISSION_GRANTED->{
+//                    Toast.makeText(activity,"$name permission granted", Toast.LENGTH_SHORT).show()
+//                }
+//                shouldShowRequestPermissionRationale(permission)-> showDialog(permission,name,requestCode)
+//                else->{
+//
+//                    ActivityCompat.requestPermissions(context as Activity, arrayOf(permission),requestCode)
+//                }
+//            }
+//        }
+//    }
+//
+//
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        fun innerCheck(name: String){
+//            if(grantResults.isEmpty()||grantResults[0] != PackageManager.PERMISSION_GRANTED){
+//                Toast.makeText(context,"$name permission refused", Toast.LENGTH_SHORT).show()
+//            }else{
+//
+//                Toast.makeText(context,"$name permission granted", Toast.LENGTH_SHORT).show()
+//
+//            }
+//        }
+//        when(requestCode){
+//            FINE_LOCATION_RQ -> innerCheck("location")
+//        }
+//    }
+//    private fun showDialog(permission:String, name : String, requestCode :Int){
+//        val builder = AlertDialog.Builder(context as Activity)
+//        builder.apply {
+//            setMessage("Permission to access your $name is required")
+//            setTitle("Permission Required")
+//            setPositiveButton("OK"){ dialog, which -> ActivityCompat.requestPermissions(context as Activity, arrayOf(permission),requestCode)
+//            }
+//            val dialog = builder.create()
+//            dialog.show()
+//        }
+//    }
 }
