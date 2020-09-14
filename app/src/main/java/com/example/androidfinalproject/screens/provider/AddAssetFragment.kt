d9bce2beda81dc4a72e1ac9015da987c9ad8ed39
juanaf31 <50581538+juanaf31@users.noreply.github.com>
2020-09-14 22:10:58 +0700

    [ADD]Add Asset

--- app/src/main/java/com/example/androidfinalproject/screens/provider/AddAssetFragment.kt
+++ app/src/main/java/com/example/androidfinalproject/screens/provider/AddAssetFragment.kt
@@ -1,17 +1,22 @@
 package com.example.androidfinalproject.screens.provider
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.BitmapFactory
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.Looper
 import android.provider.MediaStore
 import android.text.Editable
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +24,7 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import com.example.androidfinalproject.MyApplication
 import com.example.androidfinalproject.R
@@ -26,6 +32,12 @@ import com.example.androidfinalproject.provider.home.Asset
 import com.example.androidfinalproject.provider.home.ProviderHomeViewModel
 import com.example.androidfinalproject.provider.profile.ProviderProfileViewModel
 import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_add_asset.*
 import kotlinx.android.synthetic.main.fragment_profile_provider.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -37,15 +49,25 @@ import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
 
-class AddAssetFragment : Fragment(), View.OnClickListener {
+class AddAssetFragment : Fragment(), View.OnClickListener,OnMapReadyCallback,GoogleMap.OnMapClickListener {
     @Inject
     lateinit var providerHomeViewModel: ProviderHomeViewModel
     var sharedPreferences: SharedPreferences? = null
     val OPEN_CAMERA_REQUEST_CODE = 13
     val SELECT_FILE_FORM_STORAGE = 66
+    val FINE_LOCATION_RQ = 101
     lateinit var currentPhotoPath: String
     lateinit var photoFile: File
+    lateinit var gmap : GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        add_asset_map_view.onCreate(savedInstanceState)
+        add_asset_map_view.onResume()
+        add_asset_map_view.getMapAsync(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity?.applicationContext as MyApplication).applicationComponent.inject(this)
@@ -53,6 +75,7 @@ class AddAssetFragment : Fragment(), View.OnClickListener {
             getString(R.string.shared_preference_name),
             Context.MODE_PRIVATE
         )
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity as Activity)
     }
 
     override fun onCreateView(
@@ -248,5 +271,117 @@ class AddAssetFragment : Fragment(), View.OnClickListener {
 //                    }).show()
             }
         }
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            gmap = it
+            getLastLocation()
+        }
+    }
+
+
+
+    private fun checkPermission():Boolean{
+        if(
+            ActivityCompat.checkSelfPermission(context as Activity,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(context as Activity, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ){
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == FINE_LOCATION_RQ){
+            if(grantResults.isEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Log.d("Debug","You Have The Permission")
+            }
+        }
+    }
+
+    private fun getLastLocation(){
+        if(checkPermission()){
+            if(isLocationEnabled()){
+                fusedLocationClient.lastLocation.addOnCompleteListener{task->
+                    var deviceLocation = task.result
+                    if(deviceLocation == null){
+                        getNewLocation()
+                    } else{
+                        gmap.isMyLocationEnabled = true
+                        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                            // Got last known location. In some rare situations this can be null.
+                            // 3
+                            if (location != null) {
+                                deviceLocation = location
+                                val currentLatLng = LatLng(location.latitude, location.longitude)
+                                gmap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+
+                                with(sharedPreferences?.edit()) {
+                                    this?.putString(
+                                        getString(R.string.device_latitude),
+                                        location.latitude.toString()
+                                    )
+                                    this?.putString(
+                                        getString(R.string.device_longitude),
+                                        location.longitude.toString()
+                                    )
+                                    this?.commit()
+                                }
+                            }
+                        }
+                    }
+                }
+            }else{
+                Toast.makeText(context,"Please Enable Your Location Service", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(activity as Activity, arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION),FINE_LOCATION_RQ)
+    }
+
+    private fun getNewLocation(){
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2
+        checkPermission()
+        fusedLocationClient!!.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
+    }
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+            var lastLocation = p0.lastLocation
+//            deviceLocation=lastLocation
+        }
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    override fun onMapClick(p0: LatLng?) {
+//        longitudeInput.text.toString() = p0.longitude.toString()
+
+//        gmap.clear()
+        gmap.addMarker(MarkerOptions().position(p0!!))
+        println("latitude"+ p0.latitude.toString())
+        println("longitude"+ p0.longitude.toString())
     }
 }
