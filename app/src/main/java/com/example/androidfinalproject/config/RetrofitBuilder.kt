bdeb05ce33648593683e5b2166ffe386524ce134
Dinda Maharani <34880692+DindaMhn@users.noreply.github.com>
2020-09-15 20:37:38 +0700

    [UPDATE] layout add asset

--- app/src/main/java/com/example/androidfinalproject/config/RetrofitBuilder.kt
+++ app/src/main/java/com/example/androidfinalproject/config/RetrofitBuilder.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitBuilder {
     companion object{
-        val BASE_URL = "http://10.0.2.2:8080/"
+        val BASE_URL = "http://34.101.198.49:8086/"
 
         fun createRetrofit(): Retrofit {
             val retrofit = Retrofit.Builder()
